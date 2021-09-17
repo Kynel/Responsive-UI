22be9c660badaac0b7dc65844cf2d60b526fb804
kynel <kynel95@gmail.com>
2021-09-17 10:50:10 +0900

    make grid template using media query

---
+++ responsive-grid-view/src/App.js
@@ -0,0 +1,13 @@
+import Global from "./styles/global";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Global />
+      <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ responsive-grid-view/src/pages/Home/HomeStyles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import px2vw from "../../utils/px2vw";
+
+export const Header = styled.header`
+  padding: 1rem;
+  border: 1px solid red;
+`;
+
+export const ListWrapper = styled.div`
+  width: 25%;
+  float: left;
+  padding: 15px;
+  border: 1px solid red;
+`;
+
+export const Main = styled.div`
+  width: 75%;
+  float: left;
+  padding: 15px;
+  border: 1px solid red;
+`;
